e91b9ef38e6304fc07d002b3ff9068dd58adbea0
starchaser <starchaser@starchasers-MacBook-Pro.local>
2022-08-17 02:31:59 +0900

    Main page is now correctly identified

--- VisAloud/code.ts
+++ VisAloud/code.ts
@@ -39,6 +39,7 @@
 /*===== Global Variables =====*/
 const debug = true;
 
+let initialPageId: String;
 let initialFrameId: String;
 let initialFrameGlobal: FrameNode;
 let keycount = 0; // key에 들어갈 값 
@@ -69,6 +70,121 @@ function ObjCompare(obj1, obj2) {
   //   console.log('Height:', obj1.Height, '->', obj2.Height);
   // }
 };
+
+function deepCompare () {
+  var i, l, leftChain, rightChain;
+
+  function compare2Objects (x, y) {
+    var p;
+
+    // remember that NaN === NaN returns false
+    // and isNaN(undefined) returns true
+    if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
+         return true;
+    }
+
+    // Compare primitives and functions.     
+    // Check if both arguments link to the same object.
+    // Especially useful on the step where we compare prototypes
+    if (x === y) {
+        return true;
+    }
+
+    // Works in case when functions are created in constructor.
+    // Comparing dates is a common scenario. Another built-ins?
+    // We can even handle functions passed across iframes
+    if ((typeof x === 'function' && typeof y === 'function') ||
+       (x instanceof Date && y instanceof Date) ||
+       (x instanceof RegExp && y instanceof RegExp) ||
+       (x instanceof String && y instanceof String) ||
+       (x instanceof Number && y instanceof Number)) {
+        return x.toString() === y.toString();
+    }
+
+    // At last checking prototypes as good as we can
+    if (!(x instanceof Object && y instanceof Object)) {
+        return false;
+    }
+
+    if (x.isPrototypeOf(y) || y.isPrototypeOf(x)) {
+        return false;
+    }
+
+    if (x.constructor !== y.constructor) {
+        return false;
+    }
+
+    if (x.prototype !== y.prototype) {
+        return false;
+    }
+
+    // Check for infinitive linking loops
+    if (leftChain.indexOf(x) > -1 || rightChain.indexOf(y) > -1) {
+         return false;
+    }
+
+    // Quick checking of one object being a subset of another.
+    // todo: cache the structure of arguments[0] for performance
+    for (p in y) {
+        if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+            return false;
+        }
+        else if (typeof y[p] !== typeof x[p]) {
+            return false;
+        }
+    }
+
+    for (p in x) {
+        if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+            return false;
+        }
+        else if (typeof y[p] !== typeof x[p]) {
+            return false;
+        }
+
+        switch (typeof (x[p])) {
+            case 'object':
+            case 'function':
+
+                leftChain.push(x);
+                rightChain.push(y);
+
+                if (!compare2Objects (x[p], y[p])) {
+                    return false;
+                }
+
+                leftChain.pop();
+                rightChain.pop();
+                break;
+
+            default:
+                if (x[p] !== y[p]) {
+                    return false;
+                }
+                break;
+        }
+    }
+
+    return true;
+  }
+
+  if (arguments.length < 1) {
+    return true; //Die silently? Don't know how to handle such case, please help...
+    // throw "Need two or more arguments to compare";
+  }
+
+  for (i = 1, l = arguments.length; i < l; i++) {
+
+      leftChain = []; //Todo: this can be cached
+      rightChain = [];
+
+      if (!compare2Objects(arguments[0], arguments[i])) {
+          return false;
+      }
+  }
+
+  return true;
+}
 
 /*===== Node to Object =====*/
 const nodeToObject = (node) => {
@@ -263,7 +379,8 @@ async function onSelections() {
   /*===== 20220728 version: =====*/
   // clone initialFrameSelection into the Frame_store page
   cloneInPage('frame_store', initialFrameSelection);
-  changeCurrentPage('Wireframing in Figma');
+  // console.log('on change; initialPage is:', figma.root.children[0].name);
+  changeCurrentPage(figma.root.children[0].name);
   }
   return
 }
@@ -272,6 +389,8 @@ async function onSelections() {
 /*===== Run & Close Functions =====*/
 async function onRun() {
   figma.showUI(__html__);
+  let initialPageId = figma.currentPage.name;
+  console.log('initialPage is: ', initialPageId);
   let initialFrame = figma.currentPage.selection[0]
   if(debug) { console.log('initialFrame', initialFrame); }
   if (initialFrame.type != 'FRAME') { 
